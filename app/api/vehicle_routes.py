4b63a3f7b2150b76835fd219324b7dfdfaba6e7a
Daniel Lewis <daniel.c.lewis@outlook.com>
2023-09-07 12:11:47 -0500

    Vehicle route api created for / and /:id

---
+++ app/api/vehicle_routes.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, jsonify
+from flask_login import login_required
+from app.models import Vehicle
+
+vehicle_routes = Blueprint('vehicles', __name__)
+
+
+@vehicle_routes.route('/')
+def vehicles():
+    """
+    Query for all vehicles and returns them in a list of vehicle dictionaries
+    """
+    vehicles = Vehicle.query.all()
+    return {'vehicles': [vehicle.to_dict() for vehicle in vehicles]}
+
+
+@vehicle_routes.route('/<int:id>')
+@login_required
+def vehicle(id):
+    """
+    Query for a vehicle by id and returns that vehicle in a dictionary
+    """
+    vehicle = Vehicle.query.get(id)
+    return vehicle.to_dict()
