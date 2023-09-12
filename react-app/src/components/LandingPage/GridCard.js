5f9382383b9027407c6d03004557399577661cee
Daniel Lewis <daniel.c.lewis@outlook.com>
2023-09-12 16:49:32 -0500

    Background image and grid card

---
+++ react-app/src/components/LandingPage/GridCard.js
@@ -0,0 +1,11 @@
+function GridCard () {
+    return (
+        <>
+        <div className="grid-card-container">
+            <h1>Grid card</h1>
+        </div>
+        </>
+    )
+}
+
+export default GridCard
