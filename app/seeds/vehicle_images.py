e3323255e388623c051bc44853e3dcff374ab1b6
Daniel Lewis <daniel.c.lewis@outlook.com>
2023-09-17 09:20:51 -0500

    Top Ten feature

--- app/seeds/vehicle_images.py
+++ app/seeds/vehicle_images.py
@@ -26,7 +26,8 @@ def seed_vehicle_images():
                 user_id = fake.pyint(min_value=1, max_value=num_of_users),
                 vehicle_id = vehicle.id
             )
-            db.session.add(vehicle_image1)
+            print(vehicle_image1)
+            db.session.add(vehicle_image1['image_url'])
             db.session.commit()
         if len(vehicle.vehicle_images) < 2:
             response = requests.get(f"https://api.unsplash.com/search/photos?client_id=OsLCtgt819go9Q71ENBYUh5-DK7kiWPBiy8mwXUatq0&query={vehicle.make}+{vehicle.model}&orientation=portrait&per_page=1",timeout=10)
@@ -38,6 +39,7 @@ def seed_vehicle_images():
                     user_id = fake.pyint(min_value=1, max_value=num_of_users),
                     vehicle_id = vehicle.id
                     )
+                    print(vehicle_image2['image_url'])
                     db.session.add(vehicle_image2)
                     db.session.commit()
 
--- react-app/src/components/LandingPage/TopTen.js
+++ react-app/src/components/LandingPage/TopTen.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getVehicle } from "../../store/vehicle";
@@ -8,15 +8,16 @@ import "slick-carousel/slick/slick-theme.css";
 import "./LandingPage.css";
 
 export default function TopTen({ isLoaded }) {
-    const dispatch = useDispatch()
-    const [errors, setErrors] = useState({})
-    const [topTen, setTopTen] = useState([])
+	const history = useHistory();
+	const dispatch = useDispatch();
+	const [errors, setErrors] = useState({});
+	const [topTen, setTopTen] = useState([]);
 
 	useEffect(async () => {
 		if (isLoaded) {
 			const topTenResults = await fetch(`/api/dougscores/topten`);
-            const data = await topTenResults.json();
-            if (data.errors) {
+			const data = await topTenResults.json();
+			if (data.errors) {
 				setErrors(data.errors);
 			} else {
 				setTopTen(data);
@@ -25,23 +26,62 @@ export default function TopTen({ isLoaded }) {
 	}, []);
 
 	const settings = {
-		slidesToShow: 10,
+		slidesToShow: 6,
 		slidesToScroll: 1,
 		infinite: true,
 	};
 
 	return (
-		<>
+		<div className="list-card">
 			<span className="title">Top Ten</span>
 			<Slider {...settings}>
 				{topTen?.top_ten?.map((top, idx) => {
 					return (
-						<div>
-                            <h1>{idx + 1} = {top.vehicle.year}</h1>
+						<div key={top.id}>
+							<div className="list-card-container">
+								<div className="list-card-img">
+									<img
+										style={{ width: `9.5rem`, height: `14rem`, objectFit: `cover` }}
+										src={top?.vehicle?.images[1]?.image_url}
+									/>
+								</div>
+								<div className="list-card-text">
+									<div className="list-card-top">
+                                    <span><i
+											style={{ fontSize: `14px` }}
+											className="green-text fa-solid fa-star"
+										/>
+										&nbsp;{top?.dougscore_total}</span>
+                                        <span><span style={{fontSize: `12px`}}>#</span><span style={{fontSize: `18px`}}>{idx + 1}</span></span>
+									</div>
+										<div className="list-card-title">
+											<span>
+												{top?.vehicle?.make} {top?.vehicle?.model}
+											</span>
+										</div>
+									<div className="list-card-bottom">
+										<div>
+											<a href={top?.vehicle?.dougscore?.video_link}>
+												<i
+													style={{ fontSize: `14px` }}
+													className="white-text fa-solid fa-play"
+												/>
+												&nbsp;Review
+											</a>
+										</div>
+										<div>
+											<i
+												onClick={() => history.push(`/vehicles/${top?.vehicle?.id}`)}
+												className="cursor-pointer fa-solid fa-circle-info"
+											></i>
+										</div>
+									</div>
+								</div>
+							</div>
 						</div>
 					);
 				})}
 			</Slider>
-		</>
+		</div>
 	);
 }
