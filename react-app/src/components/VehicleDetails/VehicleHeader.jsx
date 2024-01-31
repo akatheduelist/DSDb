e9e196169783d6547265bf44cbfa595990be7518
Daniel Lewis <daniel.c.lewis@outlook.com>
2024-01-30 21:55:06 -0600

    Refactoring VehicleHeader css.

--- react-app/src/components/VehicleDetails/VehicleHeader.jsx
+++ react-app/src/components/VehicleDetails/VehicleHeader.jsx
@@ -65,20 +65,24 @@ function VehicleHeader({ vehicle, sessionUser }) {
   }
 
   const features = [
-    { name: 'Daily Score', description: [ `Features ${vehicle?.dougscore?.daily_features}`, 
-                                          `Comfort ${vehicle?.dougscore?.daily_comfort}`,
-                                          `Quality ${vehicle?.dougscore?.daily_quality}`,
-                                          `Practicality ${vehicle?.dougscore?.daily_practicality}`,
-                                          `Value ${vehicle?.dougscore?.daily_value}`
-                                        ] },
-    { name: 'Weekend Score', description: [ `Styling ${vehicle?.dougscore?.weekend_styling}`, 
-                                            `Acceleration ${vehicle?.dougscore?.weekend_acceleration}`,
-                                            `Handling ${vehicle?.dougscore?.weekend_handling}`,
-                                            `Fun Factor ${vehicle?.dougscore?.weekend_funfactor}`,
-                                            `Cool Factor ${vehicle?.dougscore?.weekend_coolfactor}`
-                                          ] },
+    {
+      name: 'Daily Score', description: [`Features ${vehicle?.dougscore?.daily_features}`,
+      `Comfort ${vehicle?.dougscore?.daily_comfort}`,
+      `Quality ${vehicle?.dougscore?.daily_quality}`,
+      `Practicality ${vehicle?.dougscore?.daily_practicality}`,
+      `Value ${vehicle?.dougscore?.daily_value}`
+      ]
+    },
+    {
+      name: 'Weekend Score', description: [`Styling ${vehicle?.dougscore?.weekend_styling}`,
+      `Acceleration ${vehicle?.dougscore?.weekend_acceleration}`,
+      `Handling ${vehicle?.dougscore?.weekend_handling}`,
+      `Fun Factor ${vehicle?.dougscore?.weekend_funfactor}`,
+      `Cool Factor ${vehicle?.dougscore?.weekend_coolfactor}`
+      ]
+    },
   ]
-
+  // console.log("sessionUser", sessionUser)
   return (
     <>
       <div className="bg-white">
@@ -88,7 +92,71 @@ function VehicleHeader({ vehicle, sessionUser }) {
             <p className="mt-4 text-gray-500">
               {vehicle?.description}
             </p>
-
+            <div className="flex">
+              <div>
+              {Object.values(vehicle?.tags).map(({ tag, id }) => (
+                <div key={id}>
+                  <span className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+                    {tag}
+                    {sessionUser ? (
+                  <button
+                    onClick={() => deleteTag(id)}
+                  >
+                    &nbsp;
+                    <i
+                      className='text-red-700 fa-solid fa-delete-left'
+                    />
+                  </button>
+                    ): null}
+                  </span>
+                </div>
+              ))}
+              </div>
+              {newTag ? (
+                <>
+                  <form
+                    style={{
+                      display: `inline-flex`,
+                      alignItems: `center`,
+                      paddingLeft: `12px`
+                    }}
+                    onSubmit={handleTag}
+                  >
+                    <select
+                      value={newTagData}
+                      onChange={e => setNewTagData(e.target.value)}
+                    >
+                      <option style={{ height: `100%` }} disabled>
+                        Select a tag
+                      </option>
+                      {Object.values(tags?.vehicle_tags).map(({ tag, id }) => (
+                        <option key={id} value={id}>
+                          {tag}
+                        </option>
+                      ))}
+                    </select>
+                    <button
+                      style={{ marginLeft: `4px` }}
+                      className='green-background small-bold'
+                      type='submit'
+                    >
+                      Submit
+                    </button>
+                  </form>
+                </>
+              ) : null}
+              {sessionUser && !newTag ? (
+                <button
+                  style={{ marginLeft: `1rem` }}
+                  className='no-button green-text small-bold'
+                  onClick={() => {
+                    setNewTag(!newTag)
+                  }}
+                >
+                  + Add Tag
+                </button>
+              ) : null}
+            </div>
             <dl className="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
               {features.map((feature) => (
                 <div key={feature.name} className="border-t border-gray-200 pt-4">
@@ -101,26 +169,34 @@ function VehicleHeader({ vehicle, sessionUser }) {
             </dl>
           </div>
           <div className="grid grid-cols-2 grid-rows-2 gap-4 sm:gap-6 lg:gap-8">
-            <img
-              src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-01.jpg"
-              alt="Walnut card tray with white powder coated steel divider and 3 punchout holes."
-              className="rounded-lg bg-gray-100"
-            />
-            <img
-              src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-02.jpg"
-              alt="Top down view of walnut card tray with embedded magnets and card groove."
-              className="rounded-lg bg-gray-100"
-            />
-            <img
-              src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-03.jpg"
-              alt="Side of walnut card tray with card groove and recessed card area."
-              className="rounded-lg bg-gray-100"
-            />
-            <img
-              src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-04.jpg"
-              alt="Walnut card tray filled with cards and card angled in dedicated groove."
-              className="rounded-lg bg-gray-100"
-            />
+            <a href={vehicle?.images[0]?.image_url}>
+              <img
+                src={vehicle?.images[0]?.image_url}
+                alt={vehicle?.model}
+                className="h-64 w-64 object-cover rounded-lg bg-gray-100"
+              />
+            </a>
+            <a href={vehicle?.images[1]?.image_url}>
+              <img
+                src={vehicle?.images[1]?.image_url}
+                alt={vehicle?.model}
+                className="h-64 w-64 object-cover rounded-lg bg-gray-100"
+              />
+            </a>
+            <a href={vehicle?.images[2]?.image_url}>
+              <img
+                src={vehicle?.images[2]?.image_url}
+                alt={vehicle?.model}
+                className="h-64 w-64 object-cover rounded-lg bg-gray-100"
+              />
+            </a>
+            <a href={vehicle?.images[3]?.image_url}>
+              <img
+                src={vehicle?.images[3]?.image_url}
+                alt={vehicle?.model}
+                className="h-64 w-64 object-cover rounded-lg bg-gray-100"
+              />
+            </a>
           </div>
         </div>
       </div>
